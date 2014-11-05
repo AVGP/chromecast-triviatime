211179a8c25c3f8023847f72f8eec6a812535047
Martin Naumann <martin@geekonaut.de>
2014-11-05 19:10:24 +0100

    A few questions added.

---
+++ questions.js
@@ -0,0 +1,27 @@
+var QUESTIONS = [
+  {
+    question: '2009 was the Chinese Year of the what?',
+    choices: ['Rat', 'Snake', 'Ox', 'Dragon' ],
+    correct: 'C'
+  },
+  {
+    question: 'Leoprine relates to which animals?',
+    choices: ['Leopards', 'Hares', 'Goats', 'Squirrels' ],
+    correct: 'B'
+  },
+  {
+    question: 'What is the capital of Switzerland?',
+    choices: ['Basel', 'Zurich', 'Geneva', 'Bern' ],
+    correct: 'D'
+  },
+  {
+    question: 'In the human body what is the hallux',
+    choices: ['Big Toe', 'Throat', 'Pointing finger', 'Forehead' ],
+    correct: 'A'
+  },
+  {
+    question: 'The Stanislawski method is associated with which profession?',
+    choices: ['Chemistry', 'Painting', 'Medicine', 'Acting' ],
+    correct: 'D'
+  }
+]
